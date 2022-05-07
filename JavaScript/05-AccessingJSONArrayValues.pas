bf9ffa261acbbd0de7ecd1a7f16c4b278f4326aa
500Foods <41052272+500Foods@users.noreply.github.com>
2022-05-06 17:27:35 -0700

    Update 05-AccessingJSONArrayValues.pas

--- JavaScript/05-AccessingJSONArrayValues.pas
+++ JavaScript/05-AccessingJSONArrayValues.pas
@@ -3,9 +3,10 @@ const
   SampleArrayData = '[{"name":"apple","type":"fruit"},{"name":"banana","type":"fruit"},{"name":"orange","type":"fruit"},{"name":"carrot","type":"vegetable"},{"name":"potato","type":"vegetable"}]';
   
 begin
-  asm var JS_Array = JSON.parse(SampleArrayData); end;
-
-  asm console.log('JS Array[3] is: '+JS_Array[3].name+' / '+JS_Array[3].type); end;
+  asm 
+    var JS_Array = JSON.parse(SampleArrayData); 
+    console.log('JS Array[3] is: '+JS_Array[3].name+' / '+JS_Array[3].type);
+  end;
 end;
 
 // console.log output: 
