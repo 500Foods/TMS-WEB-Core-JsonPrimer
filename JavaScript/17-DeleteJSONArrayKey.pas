4a6f04c4f7a5807b4b39f46736ccdddb7aafcf06
500Foods <41052272+500Foods@users.noreply.github.com>
2022-05-06 21:27:51 -0700

    Create 17-DeleteJSONArrayKey.pas

---
+++ JavaScript/17-DeleteJSONArrayKey.pas
@@ -0,0 +1,16 @@
+procedure TForm1.WebButton1Click(Sender: TObject);
+const
+  SampleArrayData = '[{"name":"apple","type":"fruit"},{"name":"banana","type":"fruit"},{"name":"orange","type":"fruit"},{"name":"carrot","type":"vegetable"},{"name":"potato","type":"vegetable"}]';
+  
+begin
+  asm 
+    var JS_Array = JSON.parse(SampleArrayData);
+
+    JS_Array.splice(JS_Array.findIndex(obj => obj.name == 'carrot'),1);
+
+    console.log('JS array = '+JSON.stringify(JS_Array)); 
+  end;
+end;
+
+// console.log output:
+// JS array = [{"name":"apple","type":"fruit"},{"name":"banana","type":"fruit"},{"name":"orange","type":"fruit"},{"name":"potato","type":"vegetable"}]
