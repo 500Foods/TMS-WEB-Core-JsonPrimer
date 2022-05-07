537b67176fa70139f841e7cd10b44cc661fa5bb1
500Foods <41052272+500Foods@users.noreply.github.com>
2022-05-06 20:05:44 -0700

    Create 12-AddStringsToAJSONObject.pas

---
+++ Delphi/12-AddStringsToAJSONObject.pas
@@ -0,0 +1,17 @@
+procedure TForm1.WebButton1Click(Sender: TObject);
+var
+  PAS_Object: TJSONObject;
+  
+const
+  SampleObjectData = '{"apple":"fruit","banana":"fruit","orange":"fruit","carrot":"vegetable","potato":"vegetable"}';
+  
+begin
+  PAS_Object := TJSONObject.ParseJSONValue(SampleObjectData) as TJSONObject;
+ 
+  PAS_Object.AddPair('pineapple','fruit');
+  
+  console.log('PAS object = '+PAS_Object.ToString);
+end;
+
+// console.log output:
+// PAS object = {"apple":"fruit","banana":"fruit","orange":"fruit","carrot":"vegetable","potato":"vegetable","pineapple":"fruit"}
