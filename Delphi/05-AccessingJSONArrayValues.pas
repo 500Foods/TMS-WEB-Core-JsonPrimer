79dcc5468a6d85b84d4d04786f7f34312a833455
500Foods <41052272+500Foods@users.noreply.github.com>
2022-05-06 17:24:58 -0700

    Create 05-AccessingJSONArrayValues.pas

---
+++ Delphi/05-AccessingJSONArrayValues.pas
@@ -0,0 +1,16 @@
+procedure TForm1.WebButton1Click(Sender: TObject);
+var
+  PAS_Array: TJSONArray;
+  
+const
+  SampleArrayData = '[{"name":"apple","type":"fruit"},{"name":"banana","type":"fruit"},{"name":"orange","type":"fruit"},{"name":"carrot","type":"vegetable"},{"name":"potato","type":"vegetable"}]';
+  
+begin
+  PAS_Array := TJSONObject.ParseJSONValue(SampleArrayData) as TJSONArray;
+
+  console.log('PAS Array[3] is: '+((PAS_Array[3] as TJSONObject).getValue('name') as TJSONString).Value+' / '+((PAS_Array[3] as TJSONObject).getValue('type') as TJSONString).Value);
+end;
+
+// console.log output: 
+// PAS Array[3] is: carrot / vegetable
+  
