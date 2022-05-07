6b26dc2cc5abcb6663f2b7d26360b96ecbe9c69a
500Foods <41052272+500Foods@users.noreply.github.com>
2022-05-06 19:52:58 -0700

    Update 09-CheckIfJSONObjectContainsAKey.pas

--- JavaScript/09-CheckIfJSONObjectContainsAKey.pas
+++ JavaScript/09-CheckIfJSONObjectContainsAKey.pas
@@ -3,10 +3,12 @@ const
   SampleObjectData = '{"apple":"fruit","banana":"fruit","orange":"fruit","carrot":"vegetable","potato":"vegetable"}';
 
 begin
-  asm var JS_Object = JSON.parse(SampleObjectData); end;
-  asm
+  asm 
+    var JS_Object = JSON.parse(SampleObjectData);
+
     if (JS_Object['carrot']) {console.log('JS Object contains carrot')}
     else {console.log('JS Object does not contain carrot')}
+    
     if (JS_Object['Batman']) { console.log('JS Object contains Batman')}
     else {console.log('JS Object does not contain Batman')}
   end;
